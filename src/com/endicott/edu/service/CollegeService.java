20e5e5fa3b5c910bbbed46dab33aa218de259a41
mazhigbee <mhigb411@mail.endicott.edu>
2017-09-26 16:12:29 -0400

    Added my name to the list you see when you access the launch page.

--- src/com/endicott/edu/service/CollegeService.java
+++ src/com/endicott/edu/service/CollegeService.java
@@ -95,6 +95,7 @@ public class CollegeService {
         sb.append("<p><h2>Fall 2017</h2></p>");
         sb.append("<li>Allan Brockenbrough</l1>");
         sb.append("<li>Mazlin Higbee</l1>");
+
         sb.append("</body>\n");
         return(sb.toString());
     }

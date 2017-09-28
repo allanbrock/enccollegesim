d560f5e4bb11b648a2c6df23186e7f1ae5be9df8
cambleck <mblec580@mail.endicott.edu>
2017-09-28 16:17:47 -0400

    Adding Cam Bleck to College Service

--- src/com/endicott/edu/service/CollegeService.java
+++ src/com/endicott/edu/service/CollegeService.java
@@ -98,7 +98,7 @@ public class CollegeService {
         sb.append("<li>Connor Frazier</l1>");
         sb.append("<li>Jeremy Doski</li>");
         sb.append("<li>Nick Scrivani");
-        sb.append("<li>Cam Bleck");
+        sb.append("<li>Cam Bleck</li>");
         sb.append("</body>\n");
         return(sb.toString());
     }

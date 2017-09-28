cd3ac6fa40f59308e3f0cbc582f475a18f25ca52
nickdossantos <nickdos32@gmail.com>
2017-09-28 15:39:53 -0400

    I am adding myself as a collaborator.

--- src/com/endicott/edu/service/CollegeService.java
+++ src/com/endicott/edu/service/CollegeService.java
@@ -93,7 +93,7 @@ public class CollegeService {
         sb.append("<p></p>");
         sb.append("<p><h1>Contributors</h1></p>");
         sb.append("<p><h2>Fall 2017</h2></p>");
-        sb.append("<li>Allan Brockenbrough</l1>");
+        sb.append("<li>Nick Dos Santos</l1>");
         sb.append("</body>\n");
         return(sb.toString());
     }

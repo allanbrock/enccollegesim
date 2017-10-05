cde276df3c8b71c5bb8030e59cd5b9f5830a1337
cambleck <mblec580@mail.endicott.edu>
2017-10-05 10:12:36 -0400

    Students Model

---
+++ src/com/endicott/edu/models/StudentsModel.java
@@ -0,0 +1,12 @@
+package com.endicott.edu.models;
+
+import java.util.List;
+
+// created by Cam Bleck 10/3/17
+public class StudentsModel {
+    private List<StudentModel> studentList;
+
+    public List<StudentModel> getStudentList() {return studentList; }
+
+    public void setStudentList(List<StudentModel> studentList) { this.studentList = studentList; }
+}

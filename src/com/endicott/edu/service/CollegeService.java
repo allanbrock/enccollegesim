18fb349f01527bae082b0f1b85cd33420b4c13b4
Craig <dyann490@mail.endicott.edu>
2017-10-05 16:23:18 -0400

    I added my name - Derek

--- src/com/endicott/edu/service/CollegeService.java
+++ src/com/endicott/edu/service/CollegeService.java
@@ -93,7 +93,16 @@ public class CollegeService {
         sb.append("<p></p>");
         sb.append("<p><h1>Contributors</h1></p>");
         sb.append("<p><h2>Fall 2017</h2></p>");
-        sb.append("<li>Allan Brockenbrough</l1>");
+        sb.append("<li>Mazlin Higbee</l1>");
+        sb.append("<li>Nick Dos Santos</l1>");
+        sb.append("<li>Jeremy Doski</l1>");
+        sb.append("<li>Cam Bleck</l1>");
+        sb.append("<li>Connor Frazier</l1>");
+        sb.append("<li>Allison Flood</l1>");
+        sb.append("<li>Karen Litwinczyk</l1>");
+        sb.append("<li>Nick Scrivani</l1>");
+        sb.append("<li>Chris Seidl</l1>");
+        sb.append("<li>Jeff Thor</l1>");
         sb.append("<li>Derek Yannone</l1>");
         sb.append("</body>\n");
         return(sb.toString());

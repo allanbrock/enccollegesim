52f77b203a393134d61011ac6e12f1c7b1472f9a
jdosk <jdosk314@mail.endicott.edu>
2017-10-17 16:45:49 -0400

    Fixed sportManager excess code

--- src/com/endicott/edu/simulators/SportManager.java
+++ src/com/endicott/edu/simulators/SportManager.java
@@ -2,10 +2,8 @@ package com.endicott.edu.simulators;
 
 import com.endicott.edu.datalayer.NewsFeedDao;
 import com.endicott.edu.datalayer.SportsDao;
-import com.endicott.edu.models.NewsFeedItemModel;
-import com.endicott.edu.models.NewsType;
 import com.endicott.edu.models.SportModel;
-import com.endicott.edu.models.SportsModel;
+
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -26,7 +24,7 @@ public class SportManager {
     }
 
     private void billRunningCostofSport(String runId, int hoursAlive, SportModel sport) {
-        int newCharge = (hoursAlive - sport.getHourLastUpdated()) * sport.getCostPerHour();
+        int newCharge = ((hoursAlive - sport.getHourLastUpdated()) * sport.getCostPerHour()) / 24;
         if(newCharge > 0)
         {
             Accountant.payBill(runId, newCharge);
@@ -35,42 +33,31 @@ public class SportManager {
     }
 
     public static SportModel addNewTeam(String sportName, String runId){
+        SportsDao newSportDao = new SportsDao();
         logger.info("Attempt to add sport: '" + sportName + "' to '" + runId + "'");
         SportModel result = null;
 
         if (sportName.equals("Men's Basketball")){
             result = new SportModel(12, 0, 20, 100, 0, 0, 0, 20, 50000, 0, 0, "Men's Basketball", runId, false);
-            SportsDao sportMBasketballDao = new SportsDao();
-            sportMBasketballDao.saveNewSport(runId, result);
         }
         else if(sportName.equals("Women's Basketball")){
             result  = new SportModel(12, 0, 20, 100, 0,0,0,20,50000,0,0,"Women's Basketball", runId, false);
-            SportsDao sportWBasketballDao = new SportsDao();
-            sportWBasketballDao.saveNewSport(runId, result );
         }
         else if(sportName.equals("Baseball")){
             result  = new SportModel(16, 0, 25, 100, 0,0,0,20,75000,0,0,"Baseball", runId, false);
-            SportsDao sportBaseballDao = new SportsDao();
-            sportBaseballDao.saveNewSport(runId, result );
         }
         else if(sportName.equals("Softball")){
             result  = new SportModel(16, 0, 25, 100, 0,0,0,20,75000,0,0,"Women's Basketball", runId, false);
-            SportsDao sportSoftballDao = new SportsDao();
-            sportSoftballDao.saveNewSport(runId, result );
         }
         else if(sportName.equals("Women's Soccer")){
             result  = new SportModel(15,0, 30, 10, 0, 0, 0 , 0 , 0, 14, 100, "Women's Soccer", runId, false );
-            SportsDao sportWSoccerDao = new SportsDao();
-            sportWSoccerDao.saveNewSport(runId, result );
         }
         else if(sportName.equals("Men's Soccer")){
             result  = new SportModel(15,0, 30, 10, 0, 0, 0 , 0 , 0, 14, 100, "Men's Soccer", runId, false );
-            SportsDao sportDao = new SportsDao();
-            sportDao.saveNewSport(runId, result );
         } else {
             logger.severe("Could not add sport: '" + sportName + "'");
         }
-
+        newSportDao.saveNewSport(runId, result);
         return result;
     }
 

8b5f8921db54a757cfca0447525a15eafb6eb7ed
Craig <dyann490@mail.endicott.edu>
2017-10-31 13:54:35 -0400

    Adds plague service

---
+++ src/com/endicott/edu/service/PlagueService.java
@@ -0,0 +1,92 @@
+package com.endicott.edu.service;
+
+import com.endicott.edu.datalayer.PlagueDao;
+import com.endicott.edu.models.PlagueModel;
+import com.endicott.edu.simulators.CollegeManager;
+import com.google.gson.Gson;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+// The Java class will be hosted at the URI path "/plagues"
+@Path("/plagues")
+public class PlagueService {
+    private PlagueDao dao = new PlagueDao();
+
+    /**
+     * Create a new plague.
+     * Notice that it consumes "text plain".  It really should be  APPLICATION_JSON
+     * but having trouble getting this to work.
+     *
+     * @return college in JSON format
+     */
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PlagueModel postDorm(String dormJsonString) {
+        Gson g = new Gson();
+        PlagueModel plague = g.fromJson(dormJsonString, PlagueModel.class);
+
+        // If we already have a plague in progress
+        // We should return an error.
+
+        // Make sure the college exists, return error if not.
+        String runId = plague.getRunId();
+        if (!CollegeManager.doesCollegeExist(runId)) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        // Override some fields
+        plague.setHourLastUpdated(0);
+
+        // Create a plague
+        PlagueDao dormDao = new PlagueDao();
+        dormDao.saveNewPlague(runId, plague);
+        plague.setNote("Plague created");
+        return plague;
+    }
+
+    /**
+     * Get a list of the plagues
+     *
+     * @param runId simulation ID
+     * @return JSON formatted list.
+     */
+    @GET
+    @Path("/{runId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PlagueModel> getPlagues(@PathParam("runId") String runId) {
+        return dao.getPlagues(runId);
+    }
+
+}
+
+//    @GET
+//    @Produces("text/plain")
+//    //@Produces(MediaType.APPLICATION_XML)
+//    public DormitoryModel getById() {
+//        DormitoryModel acct = dao.getFinances("8");
+//        return acct;
+//    }
+//    @GET
+//    @Path("/{id}")
+//    @Produces(MediaType.APPLICATION_XML)
+//    public DormitoryModel getById(final @PathParam("id") String id) {
+//        DormitoryModel acct = dao.getFinances(id);
+//        return acct;
+//    }
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getFinances() {
+//        DormitoryModel bankAccount = dao.getFinances("8");
+//        return bankAccount.toJson();
+//    }
+//- works 7/19 returns xml displayable in the browser
+//    @GET
+//    @Produces(MediaType.APPLICATION_XML)
+//    public DormitoryModel getFinances() {
+//        DormitoryModel bankAccount = dao.getFinances("8");
+//        return bankAccount;
+//    }

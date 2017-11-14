7197ee179da2e5eb50601c2d03e63096764353ca
Craig <dyann490@mail.endicott.edu>
2017-11-14 16:16:43 -0500

    Doing some Plague stuff

--- src/com/endicott/edu/simulators/CollegeManager.java
+++ src/com/endicott/edu/simulators/CollegeManager.java
@@ -63,12 +63,12 @@ public class CollegeManager {
         NewsManager.createNews(runId, college.getCurrentDay(),"Dorm " + dorm.getName() + " has opened.", NewsType.GENERAL_NOTE);
 
         // Create a plague
-        // Make students sick.
-        logger.info("Generating Plague");
-        PlagueModel plague = new PlagueModel( 0, 0, "Hampshire Hall","none", 1, 0, 1000, 72, 0);
-        PlagueDao plagueDao = new PlagueDao();
-        plagueDao.saveNewPlague(runId, plague);
-        NewsManager.createNews(runId, college.getCurrentDay(),"Dorm " + dorm.getName() + " has been infected. 1 student(s) are sick.", NewsType.GENERAL_NOTE);
+        PlagueManager plague = new PlagueManager();
+        plague.createInitialPlague(runId);
+
+
+
+
 
         SportManager sportManager = new SportManager();
         sportManager.addNewTeam("Men's Soccer", runId);

65b628898e9e4510ae152b9bbf5b1975fdf154e4
allanbrock <abrocken@endicott.edu>
2018-09-09 10:20:33 -0400

    Adding a new year

--- src/com/endicott/edu/service/CollegeService.java
+++ src/com/endicott/edu/service/CollegeService.java
@@ -109,6 +109,7 @@ public class CollegeService {
         sb.append("<body>\n");
         sb.append("<p></p>");
         sb.append("<p><h1>Contributors</h1></p>");
+        sb.append("<p><h2>Founders: Fall... 2018</h2></p>");
         sb.append("<p><h2>Founders: Fall 2017</h2></p>");
         sb.append("<li>Mazlin Higbee</l1>");
         sb.append("<li>Nick Dos Santos</l1>");
